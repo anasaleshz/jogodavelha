ef365aff0d7dde1d62b21bf21436f53679278b91
anasaleshz <143184784+anasaleshz@users.noreply.github.com>
2023-11-17 09:16:22 -0300

    final

--- jogo/jogo.js
+++ jogo/jogo.js
@@ -6,3 +6,23 @@ let turno;
 
 const JOGADOR_X = "X";
 const JOGADOR_0 = "0";
+
+let jogoacabou = false;
+
+document.addEventListener("click". (event) => {
+    if (event.target.matches(".celula") && !jogoacabou) {
+        JOGADOR_0(event.target.id);
+    }
+});
+
+function jogar(id) {
+    const celula = document.getElementById(id);
+    if (celula.textContent ==="") {
+        turno = checarTurno ? JOGADOR_X : JOGADOR_0;
+        checarTurno = !checarTurno;
+        celula.textContent = turno;
+        celula.classList.add(turno);
+        celula.removeEventListener("click", jogar);
+        verificarVencedor(turno);
+    }
+}
